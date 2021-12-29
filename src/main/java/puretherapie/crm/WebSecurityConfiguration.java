60384dfe0c453ceb4034b89dfaf44a8b68cc0c30
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2021-12-29 20:06:31 +0100

    Correctly configure csrf security

--- src/main/java/puretherapie/crm/WebSecurityConfiguration.java
+++ src/main/java/puretherapie/crm/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import puretherapie.crm.person.user.data.User;
 
 import static puretherapie.crm.api.v1.client.ClientController.API_V1_CLIENT_URL;
@@ -20,22 +21,42 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
-                .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .and()
-                .httpBasic()
-                .and()
-                .authorizeRequests()
+        configureCors(http);
+        configureCsrf(http);
+        configureAuthorizeRequests(http);
+        configureSession(http);
+        configureHttpBasic(http);
+        configureLoginLogout(http);
+    }
+
+    private void configureCors(HttpSecurity http) throws Exception {
+        http.cors().disable();
+    }
+
+    private void configureCsrf(HttpSecurity http) throws Exception {
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(API_V1_USER_URL + "/**");
+    }
+
+    private void configureSession(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+    }
+
+    private void configureHttpBasic(HttpSecurity http) throws Exception {
+        http.httpBasic();
+    }
+
+    private void configureAuthorizeRequests(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, API_V1_USER_URL + "/**").authenticated()
-                .antMatchers(HttpMethod.POST, API_V1_CLIENT_URL + "/registration").permitAll()
                 .antMatchers(HttpMethod.PUT, API_V1_CLIENT_URL).hasRole(String.valueOf(User.UserRole.BOSS.getLevel()))
                 .antMatchers(HttpMethod.GET, API_V1_CLIENT_URL).hasRole(String.valueOf(User.UserRole.BOSS.getLevel()))
-                .and()
-                .formLogin().disable()
-                .logout().disable()
-        ;
+                .anyRequest().authenticated();
+    }
+
+    private void configureLoginLogout(HttpSecurity http) throws Exception {
+        http.formLogin().disable()
+                .logout().disable();
     }
 
     @Bean

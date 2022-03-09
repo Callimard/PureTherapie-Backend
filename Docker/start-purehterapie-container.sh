26ab3401e5c8ec4654f584b4f661636d5b7fad42
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-03-09 01:57:44 +0100

    release/1.3.0

--- Docker/start-purehterapie-container.sh
+++ Docker/start-purehterapie-container.sh
@@ -3,4 +3,5 @@
 docker run --restart=always -d -t -p8585:8585 --network="host" \
   -v puretherapie-logs:/logs \
   -v puretherapie-properties:/configuration \
+  -v puretherapie-uploads:/uploads \
   puretherapie-backend:latest

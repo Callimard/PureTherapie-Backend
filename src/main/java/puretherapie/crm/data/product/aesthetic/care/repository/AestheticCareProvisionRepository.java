d6055ee77835897b2f64e8c7cef5a18f11674ba8
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-03-12 00:58:48 +0100

    #26ffy3f[in progress] Ajout du KPI TechnicianACProvisionKPI

--- src/main/java/puretherapie/crm/data/product/aesthetic/care/repository/AestheticCareProvisionRepository.java
+++ src/main/java/puretherapie/crm/data/product/aesthetic/care/repository/AestheticCareProvisionRepository.java
@@ -3,6 +3,7 @@ package puretherapie.crm.data.product.aesthetic.care.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import puretherapie.crm.data.appointment.Appointment;
 import puretherapie.crm.data.person.client.Client;
+import puretherapie.crm.data.person.technician.Technician;
 import puretherapie.crm.data.product.aesthetic.care.AestheticCareProvision;
 
 import java.time.LocalDateTime;
@@ -15,5 +16,8 @@ public interface AestheticCareProvisionRepository extends JpaRepository<Aestheti
     AestheticCareProvision findByClientAndAppointment(Client client, Appointment appointment);
 
     List<AestheticCareProvision> findByDateGreaterThanEqualAndDateLessThan(LocalDateTime begin, LocalDateTime end);
+
+    List<AestheticCareProvision> findByTechnicianAndDateGreaterThanEqualAndDateLessThan(Technician technician, LocalDateTime begin,
+                                                                                        LocalDateTime end);
 
 }
